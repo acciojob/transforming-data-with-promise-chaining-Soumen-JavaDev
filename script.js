366ef424a040dafbe76c9e312fc33d409141b125
SOUMEN MONDAL <151416831+Soumen-JavaDev@users.noreply.github.com>
2025-05-04 11:17:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,67 +1,72 @@
-//your JS code here. If required.
-let input =document.querySelector("#ip");
-let btn=document.querySelector("#btn");
-let output=document.querySelector("#output");
+let input = document.querySelector("#ip");
+let btn = document.querySelector("#btn");
+let output = document.querySelector("#output");
 
-btn.addEventListener("click",()=>{
-	output.innerHTML = "";
-	let val=Number(input.value.trim());
-	initial(val).then((res)=>{
-		
-		output.innerHTML+=`Result:${res}<br>`;
-		return second(res);
-	}).then((res)=>{
-		output.innerHTML+=`Result:${res}<br>`;
-		return Third(res);
-	}).then((res)=>{
-		output.innerHTML+=`Result:${res}<br>`;
-		return Fourth(res);
-	}).then((res)=>{
-		output.innerHTML+=`Result:${res}<br>`;
-		return Fifth(res);
-	}).then((res)=>{
-		output.innerHTML += `Final Result: ${res}<br>`;
+btn.addEventListener("click", () => {
+	output.innerHTML = ""; // Clear previous output
 
-	})
+	let val = Number(input.value.trim());
 
-})
-function initial(val){
-	return new Promise ((resolve,reject)=>{
-        
-		setTimeout(()=>{
+	initial(val)
+		.then((res) => {
+			output.innerHTML += `Result: ${res}<br>`;
+			return second(res);
+		})
+		.then((res) => {
+			output.innerHTML += `Result: ${res}<br>`;
+			return third(res);
+		})
+		.then((res) => {
+			output.innerHTML += `Result: ${res}<br>`;
+			return fourth(res);
+		})
+		.then((res) => {
+			output.innerHTML += `Result: ${res}<br>`;
+			return fifth(res);
+		})
+		.then((res) => {
+			output.innerHTML += `Final Result: ${res}<br>`;
+		});
+});
+
+// Each function returns a Promise with delay using setTimeout
+
+function initial(val) {
+	return new Promise((resolve) => {
+		setTimeout(() => {
 			resolve(val);
-		},2000)
-	})
+		}, 2000); // 2 seconds
+	});
 }
-function second(params) {
-	return new Promise ((resolve,reject)=>{
 
-		setTimeout(()=>{
-			resolve(params*2);
-		},2000)
-	})
+function second(val) {
+	return new Promise((resolve) => {
+		setTimeout(() => {
+			resolve(val * 2);
+		}, 2000); // 2 seconds
+	});
 }
-function Third (params) {
-	return new Promise ((resolve,reject)=>{
 
-		setTimeout(()=>{
-			resolve(params-3);
-		},1000)
-	})
+function third(val) {
+	return new Promise((resolve) => {
+		setTimeout(() => {
+			resolve(val - 3);
+		}, 1000); // 1 second
+	});
 }
-function Fourth (params) {
-	return new Promise ((resolve,reject)=>{
 
-		setTimeout(()=>{
-			resolve(params/2);
-		},1000)
-	})
+function fourth(val) {
+	return new Promise((resolve) => {
+		setTimeout(() => {
+			resolve(val / 2);
+		}, 1000); // 1 second
+	});
 }
-function Fifth (params) {
-	return new Promise ((resolve,reject)=>{
 
-		setTimeout(()=>{
-			resolve(params+10);
-		},1000)
-	})
+function fifth(val) {
+	return new Promise((resolve) => {
+		setTimeout(() => {
+			resolve(val + 10);
+		}, 1000); // 1 second
+	});
 }
